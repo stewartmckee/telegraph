9a663ae2016113691bf1baf78b58b60e0af50446
timuckun <timuckun@875c329e-13df-46a2-9bfe-4c8d66ad937e>
2007-07-04 04:24:18 +0000

    Changed install.rb to install the yaml file

--- install.rb
+++ install.rb
@@ -4,3 +4,4 @@ require 'ftools'
 %w{agi_server ami_server ami_event_handler}.each do |f|
   File.copy "#{File.dirname(__FILE__)}/lib/#{f}", "#{File.dirname(__FILE__)}/../../../script/#{f}"
 end
+File.copy "#{File.dirname(__FILE__)}/lib/telegraph.yaml", "#{File.dirname(__FILE__)}/../../../config/telegraph.yaml"
